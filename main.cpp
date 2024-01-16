3db8d9a18b8a1852146e0730c66ec4b97ee528fb
DaniilNosov <nosov281@gmail.com>
2024-01-16 02:17:54 +0200

    1

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main() {
+    const int iterations = 10000000;
+    long long sum = 0;
+    const long long stopValue = 10000000000;
+
+    for (int j = 0; j < 2; ++j) {
+        for (int i = 1; i <= iterations; ++i) {
+            sum += 2 + (i - 1) * 3;
+            std::cout << sum << std::endl;
+
+            if (sum > stopValue) {
+                std::cout << sum << std::endl;
+                return 0;
+            }
+        }
+    }
+
+    return 0;
+}
+
